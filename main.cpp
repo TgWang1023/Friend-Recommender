e68793f59fc8ff91d39cc7dd81e4262535e8407a
Tiange Wang <wtgkevin@gmail.com>
2018-11-21 17:34:36 -0800

    Input file feature

--- main.cpp
+++ main.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#include <fstream>
+#include "b-tree.h"
+#include "graph.h"
 
 using namespace std;
 
 int main() {
+    fstream word_file;
+    cout << "\n\n\nFriend Recommender" << endl;
+    cout << "Please select the action you would like to perform:" << endl;
+
+    while(1) {
+        cout << "-------------------------" << endl;
+        cout << "1: Input file" << endl;
+        cout << "2: Add a user" << endl;
+        cout << "3: Find a user" << endl;
+        cout << "4: Find a user's details" << endl;
+        cout << "5: Recommend friends" << endl;
+        cout << "0: Exit" << endl;
+        cout << "-------------------------" << endl;
+        cout << "Choice: ";
+
+        char choice;
+        cin >> choice;
+
+        switch(choice) {
+            case '1': {
+                word_file.close();
+                cout << "Please enter the name of the file: ";
+                string filename;
+                cin.ignore();
+                getline(cin, filename);
+                word_file.open(filename);
+                if (word_file.is_open()) {
+                    cout << "Successfully opened file." << endl;
+                } else {
+                    cout << "Unable to open this file." << endl;
+                }
+                break;
+            }
+            case '2': {
+                cout << "case 2" << endl;
+                break;
+            }
+            case '3': {
+                cout << "case 3" << endl;
+                break;
+            }
+            case '4': {
+                cout << "case 4" << endl;
+                break;
+            }
+            case '5': {
+                cout << "case 5" << endl;
+                break;
+            }
+            case '0': {
+                cout << "Goodbye" << endl;
+                word_file.close();
+                exit(0);
+            }
+            default:
+                cout << "Please enter 1, 2, 3, 4 or 5 to perform an action or 0 to quit" << endl;
+        }
+    }
     return 0;
 }
